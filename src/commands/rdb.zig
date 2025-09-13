94e16ebdd1a76119f74d4f014912dd6f46937ae0
Charles Fonseca <charles.fonseca@pm.me>
2025-09-13 18:28:31 -0300

    refactor: consolidate ZDB_Writer into ZDB and remove obsolete files

--- src/commands/rdb.zig
+++ src/commands/rdb.zig
@@ -4,12 +4,12 @@ const Store = store.Store;
 const ZedisObject = store.ZedisObject;
 const Client = @import("../client.zig").Client;
 const Value = @import("../parser.zig").Value;
-const ZDB_Writer = @import("../rdb/zdb_writer.zig").ZDB_Writer;
+const ZDB = @import("../rdb/zdb.zig").ZDB;
 
 pub fn save(client: *Client, args: []const Value) !void {
     _ = args;
 
-    var zdb = try ZDB_Writer.init(client.allocator, client.store, "test.rdb");
+    var zdb = try ZDB.Writer.init(client.allocator, client.store, "test.rdb");
     defer zdb.deinit();
     try zdb.writeFile();
 
